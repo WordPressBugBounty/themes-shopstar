f65c0859a9e12e6991a551a2cf433e5784e67c44
Sean Murphy <sean@iamseanmurphy.com>
2024-10-19 10:34:57 +0000

    1.1.46

---
+++ shopstar/customizer/customizer-library/includes/dynamic-css.php
@@ -0,0 +1,69 @@
+
+/* Accordion section rollover */
+#customize-theme-controls #accordion-section-colors h3:hover,
+
+#customize-theme-controls #accordion-panel-shopstar-fonts h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-site-title-fonts h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-navigation-menu-fonts h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-slider-fonts h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-heading-fonts h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-body-fonts h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-form-field-fonts h3:hover,
+
+#customize-theme-controls #accordion-section-shopstar-font-awesome h3:hover,
+
+#customize-theme-controls #accordion-panel-shopstar-styling h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-styling-links h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-styling-page-builders h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-styling-plugins h3:hover,
+
+#customize-theme-controls #accordion-section-shopstar-layout h3:hover,
+
+#customize-theme-controls #accordion-panel-shopstar-header h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-header-top-bar h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-header-site-logo-area h3:hover,
+	#customize-theme-controls #accordion-section-shopstar-header-text h3:hover,
+
+#customize-theme-controls #accordion-section-shopstar-social h3:hover,
+#customize-theme-controls #accordion-section-shopstar-search h3:hover,
+#customize-theme-controls #accordion-section-shopstar-slider h3:hover,
+#customize-theme-controls #accordion-section-shopstar-blog h3:hover,
+#customize-theme-controls #accordion-section-shopstar-404-page h3:hover,
+#customize-theme-controls #accordion-section-shopstar-gutenberg h3:hover,
+#customize-theme-controls #accordion-section-shopstar-media h3:hover {
+	background-color: <?php echo get_theme_mod( 'shopstar-primary-color', customizer_library_get_default( 'shopstar-primary-color' ) ); ?>;
+}
+
+/* Expanded Section heading colors */
+#sub-accordion-section-colors.control-section.open h3,
+#sub-accordion-panel-shopstar-fonts.control-section div.accordion-section-title,
+	#sub-accordion-section-shopstar-site-title-fonts.control-section.open h3,
+	#sub-accordion-section-shopstar-navigation-menu-fonts.control-section.open h3,
+	#sub-accordion-section-shopstar-slider-fonts.control-section.open h3,
+	#sub-accordion-section-shopstar-heading-fonts.control-section.open h3,
+	#sub-accordion-section-shopstar-body-fonts.control-section.open h3,
+	#sub-accordion-section-shopstar-form-field-fonts.control-section.open h3,
+
+#sub-accordion-section-shopstar-font-awesome.control-section.open h3,
+
+#sub-accordion-panel-shopstar-styling.control-section div.accordion-section-title,
+	#sub-accordion-section-shopstar-styling-links.control-section.open h3,
+	#sub-accordion-section-shopstar-styling-page-builders.control-section.open h3,
+	#sub-accordion-section-shopstar-styling-plugins.control-section.open h3,
+
+#sub-accordion-section-shopstar-layout.control-section.open h3,
+
+#sub-accordion-panel-shopstar-header.control-section div.accordion-section-title,
+	#sub-accordion-section-shopstar-header-top-bar.control-section.open h3,
+	#sub-accordion-section-shopstar-header-site-logo-area.control-section.open h3,
+	#sub-accordion-section-shopstar-header-text.control-section.open h3,
+
+#sub-accordion-section-shopstar-social.control-section.open h3,
+#sub-accordion-section-shopstar-search.control-section.open h3,
+#sub-accordion-section-shopstar-slider.control-section.open h3,
+#sub-accordion-section-shopstar-blog.control-section.open h3,
+#sub-accordion-section-shopstar-404-page.control-section.open h3,
+#sub-accordion-section-shopstar-gutenberg.control-section.open h3,
+#sub-accordion-section-shopstar-media.control-section.open h3 {
+	background-color: <?php echo get_theme_mod( 'shopstar-primary-color', customizer_library_get_default( 'shopstar-primary-color' ) ); ?> !important;
+}
